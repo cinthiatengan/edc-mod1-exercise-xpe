97ce1eaf5be0f37aadf761fecb5616fe068d9faa
Cinthia Tengan <cinthia.tengan@ab-inbev.com>
2022-09-03 13:47:40 -0300

    wip

---
+++ job_spark.py
@@ -0,0 +1,28 @@
+#comentario para modificar o arquivo 
+from pyspark.sql.functions import mean, max, min, col, count
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder.appName("ExerciseSpark")
+    .getOrCreate()
+)
+
+# ler os dados do enem 2020
+enem = (
+    spark
+    .read
+    .format("csv")
+    .option("header", True)
+    .option("inferSchema", True)
+    .option("delimiter", ";")
+    .load("s3://datalake-cin-edc-2022/raw-data/enem-data")
+)
+
+(
+    enem
+    .write
+    .mode("overwrite")
+    .format("parquet")
+    .partitionBy("NU_ANO")
+    .save("s3://datalake-cin-edc-2022/staging/enem")
+)
